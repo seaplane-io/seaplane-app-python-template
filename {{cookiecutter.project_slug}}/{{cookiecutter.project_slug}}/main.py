47693a65d3869a1840beae6a846ab295bab35268
tonilopezmr <toni@seaplane.io>
2023-08-22 16:24:00 +0200

    Fix lint

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/main.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/main.py
@@ -1,14 +1,16 @@
-from seaplane import app, task, config, start
+from seaplane import app, config, start, task
 
 config.set_api_key("sp-your_api_key")
 
-@task(type="compute", id='hello-world-task')
-def hello_world_task(data):
-	return "hello world"
 
-@app(id='hello-world-app', path='/hello')
+@task(type="compute", id="hello-world-task")
+def hello_world_task(data):
+    return "hello world"
+
+
+@app(id="hello-world-app", path="/hello")
 def hello_world_app(data):
-	return hello_world_task(data)
+    return hello_world_task(data)
 
 
 start()
