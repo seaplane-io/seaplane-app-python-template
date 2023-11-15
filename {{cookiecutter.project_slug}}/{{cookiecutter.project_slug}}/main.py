19398f819b23e0a7d80683a11552ce47cb79a38c
joeatwork <joeb@seaplane.io>
2023-11-15 14:35:45 -0500

    use "emit" to send data

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/main.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/main.py
@@ -3,9 +3,11 @@ from seaplane.apps import app, start, task
 #Remember to add your SEAPLANE_API_KEY in .env file
 
 @task(type="compute", id="hello-world-task")
-def hello_world_task(data):
-    return "hello world"
+def hello_world_task(context):
+    # This goes into the seaplane log
+    print(f"got context data {context.body}")
 
+    context.emit(b"hello world!")
 
 @app(id="hello-world-app")
 def hello_world_app(data):
