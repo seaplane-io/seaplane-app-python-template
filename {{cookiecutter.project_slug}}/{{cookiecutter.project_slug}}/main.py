a46b47be65bc60dcc6cf3343f7c7a4bdd2f0f6f9
joeatwork <joeb@seaplane.io>
2023-11-16 09:30:45 -0500

    Revert "nice names for tasks and apps"
    This reverts commit b38c890f531fa52187f79d83c8351417a18dd597.
    
    Names with underscores will break, this is isn't urgent enough to pursue
    right now.

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/main.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/main.py
@@ -2,14 +2,14 @@ from seaplane.apps import app, start, task
 
 #Remember to add your SEAPLANE_API_KEY in .env file
 
-@task(type="compute", id="{{cookiecutter.project_slug}}-task")
+@task(type="compute", id="hello-world-task")
 def hello_world_task(context):
     # This goes into the seaplane log
     print(f"got context data {context.body}")
 
     context.emit(b"hello world!")
 
-@app(id="{{cookiecutter.project_slug}}-app")
+@app(id="hello-world-app")
 def hello_world_app(data):
     return hello_world_task(data)
 
